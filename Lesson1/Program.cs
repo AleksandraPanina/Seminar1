09310e21f59d60c7fc94548fcefe6b04e19c2f3e
Aleksandra <canonfine90@mail.ru>
2022-12-12 17:00:00 +0300

    init project

---
+++ Lesson1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число
+// и выдает его квадрат (число умноженное на само себя).
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;
+
+Console.WriteLine(quarter);
